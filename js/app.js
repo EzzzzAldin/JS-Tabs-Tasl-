9e01bd037c6ef957d2d1027afa16e766ad7c7bba
EzzzzAldin <zezomomhamed74@gmail.com>
2021-12-29 06:59:45 +0200

    Edit 3 Slide Taps App

--- js/app.js
+++ js/app.js
@@ -1,26 +1,33 @@
 // Get All Element
+const btns = document.querySelectorAll("button");
+const headerSlide = document.querySelectorAll("h4");
 const dataCard = document.querySelector(".data");
-let i = 0;
 
-setInterval(() => {
-  dataCard.children[i].classList.add("hidden");
-  i++;
-  if (i === 3) {
-    dataCard.style.backgroundColor = "#039be569";
-    dataCard.children[0].style.color = "#0d47a1";
-    dataCard.children[3].style.color = "#0d47a1";
-  }
-  if (i === 1) {
-    dataCard.style.backgroundColor = "#a8a4a4";
-    dataCard.children[0].style.color = "#000";
-    dataCard.children[3].style.color = "#263238";
-  }
-  if (i === 2) {
-    dataCard.style.backgroundColor = "#fff";
-    dataCard.children[3].style.color = "#263238";
-  }
-  if (i >= dataCard.children.length - 1) {
-    i = 0;
-  }
-  dataCard.children[i].classList.remove("hidden");
-}, 6000);
+// Function Taps Slide
+function toggel(first, second, third) {
+  // Toggel Between Hide And Show
+  if (!second.classList.contains("hidden")) second.classList.add("hidden");
+  if (!third.classList.contains("hidden")) third.classList.add("hidden");
+  first.classList.remove("hidden");
+}
+
+btns[0].addEventListener("click", () => {
+  // Call Function Slide taps
+  toggel(headerSlide[0], headerSlide[1], headerSlide[2]);
+  //   Some Steyle
+  dataCard.style.backgroundColor = "#039be569";
+});
+
+btns[1].addEventListener("click", () => {
+  toggel(headerSlide[1], headerSlide[0], headerSlide[2]);
+  dataCard.style.backgroundColor = "#a8a4a4";
+  dataCard.children[1].style.color = "#000";
+  dataCard.children[3].style.color = "#263238";
+});
+
+btns[2].addEventListener("click", () => {
+  toggel(headerSlide[2], headerSlide[0], headerSlide[1]);
+  dataCard.style.backgroundColor = "#fff";
+  dataCard.children[2].style.color = "#a8a4a4";
+  dataCard.children[3].style.color = "#263238";
+});
